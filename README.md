fcf847b25c42c0cd9421707c09d1e3090bed246a
Philipe Souza <pessoalphilipesouza@gmail.com>
2022-07-01 19:38:29 -0300

    beta
    script e readme adicionados ao repositorio

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# whatsapp-script-music
- Envia uma musica no whatsapp verso por verso
+# Whatsapp Script Music
+**Como usar o script:**
+
+Seleciona uma conversa no whatsapp web
+
+Aperte a tecla f12 para abrir o terminal e cole o código que está no arquivo index.js
+
+Cole a letra da musica na caixa de texto que vai abrir
+
+Divirta-se :smile:
---
+++ index.js
@@ -0,0 +1,21 @@
+var musica = prompt('Cole a letra da musica aqui', '‎');
+var verso = musica.split("\n");
+var counter = parseInt(frases.length);
+window.InputEvent = window.Event || window.InputEvent;
+var event = new InputEvent('input', { bubbles: true });
+var textbox = document.getElementsByClassName(
+  '_13NKt copyable-text selectable-text',
+)[1];
+for (let index = 0; index < counter; index++) {
+  (function (ind) {
+    setTimeout(function () {
+      textbox.innerHTML = verso[index];
+      textbox.dispatchEvent(event);
+      document
+        .getElementsByClassName(
+          'tvf2evcx oq44ahr5 lb5m6g5c svlsagor p2rjqpw5 epia9gcq',
+        )[0]
+        .click();
+    }, 1000 + 1000 * ind);
+  })(index);
+}
